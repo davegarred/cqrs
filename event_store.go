3aee4adaaeb9674200ec34c664f5e5f08477dad9
Dave Garred <dave.garred@gmail.com>
2018-08-16 22:27:55 -0700

    serialize in eventstore

--- event_store.go
+++ event_store.go
@@ -1,26 +1,57 @@
 package cqrs
 
+import (
+	"reflect"
+	"encoding/json"
+)
+
 type EventStore interface {
-	Persist(string,[]Event)
-	Load(string) []Event
+	Persist(aggregateId string, events []Event)
+	Load(aggregateId string) []Event
 }
 
 type MemEventStore struct {
-	eventMap map[string][]Event
+	eventMap map[string][]StoredEvent
+}
+
+type StoredEvent struct {
+	eventType reflect.Type
+	payload []byte
 }
 
 func (s *MemEventStore) Persist(aggregateId string, newEvents []Event) {
 	events := s.eventMap[aggregateId]
 	if events == nil{
-		events = make([]Event,0)
+		events = make([]StoredEvent,0)
 	}
-	events = append(events, newEvents...)
+	for _,event := range newEvents {
+		events = append(events, serialize(event))
+	}
 	s.eventMap[aggregateId] = events
 }
 func (s *MemEventStore) Load(aggregateId string) []Event {
-	return s.eventMap[aggregateId]
+	storedEvents := s.eventMap[aggregateId]
+	events := make([]Event,len(storedEvents))
+	for i,storedEvent := range storedEvents {
+		event := reflect.New(storedEvent.eventType).Interface().(Event)
+		err := json.Unmarshal(storedEvent.payload, event)
+		if err != nil {
+			panic(err)
+		}
+		events[i] = reflect.ValueOf(event).Elem().Interface().(Event)
+	}
+	return events
 }
 
 func NewMemEventStore() EventStore {
-	return &MemEventStore{make(map[string][]Event)}
+	return &MemEventStore{make(map[string][]StoredEvent)}
+}
+
+func serialize(event Event) StoredEvent {
+	eventType := reflect.TypeOf(event)
+	payload,err := json.Marshal(event)
+	if err != nil{
+		panic(err)
+	}
+	return StoredEvent{eventType, payload}
 }
